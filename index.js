be0bb5dc38c0688afbe5ebe987aa16677ad6f29e
Bramwel <bramwel.mutngati@student.moringaschool.com>
2025-03-06 16:10:02 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(){
+    console.log('This Saturday, I want to roller-skate!');
+}
+
+function mondayWork(){
+    console.log('This Monday, I will go to the office.');
+}
+
+function saturdayFun(activity= 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+
+}
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair = '*'){
+    return function(adjective = 'special'){
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
